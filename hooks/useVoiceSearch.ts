b3f9884e0e1b23c796bc8bfb3126f163903f384f
dipesh-m12 <mavinash423@gmail.com>
2026-01-17 00:47:15 +0530

    dipesh - rn-voice@voice uses old android version - removed :

--- hooks/useVoiceSearch.ts
+++ hooks/useVoiceSearch.ts
@@ -1,44 +1,44 @@
-import Voice from '@react-native-voice/voice';
-import { useEffect, useState } from 'react';
+// import Voice from '@react-native-voice/voice';
+// import { useEffect, useState } from 'react';
 
-export const useVoiceSearch = (onResult: (text: string) => void) => {
-  const [isListening, setIsListening] = useState(false);
+// export const useVoiceSearch = (onResult: (text: string) => void) => {
+//   const [isListening, setIsListening] = useState(false);
 
-  useEffect(() => {
-    Voice.onSpeechResults = (event) => {
-      const text = event.value?.[0];
-      if (text) onResult(text);
-      setIsListening(false);
-    };
+//   useEffect(() => {
+//     Voice.onSpeechResults = (event) => {
+//       const text = event.value?.[0];
+//       if (text) onResult(text);
+//       setIsListening(false);
+//     };
 
-    Voice.onSpeechError = () => {
-      setIsListening(false);
-    };
+//     Voice.onSpeechError = () => {
+//       setIsListening(false);
+//     };
 
-    return () => {
-      Voice.destroy().then(Voice.removeAllListeners);
-    };
-  }, []);
+//     return () => {
+//       Voice.destroy().then(Voice.removeAllListeners);
+//     };
+//   }, []);
 
-  const startListening = async () => {
-    try {
-      setIsListening(true);
-      await Voice.start('en-IN');
-    } catch {
-      setIsListening(false);
-    }
-  };
+//   const startListening = async () => {
+//     try {
+//       setIsListening(true);
+//       await Voice.start('en-IN');
+//     } catch {
+//       setIsListening(false);
+//     }
+//   };
 
-  const stopListening = async () => {
-    try {
-      await Voice.stop();
-      setIsListening(false);
-    } catch {}
-  };
+//   const stopListening = async () => {
+//     try {
+//       await Voice.stop();
+//       setIsListening(false);
+//     } catch {}
+//   };
 
-  return {
-    isListening,
-    startListening,
-    stopListening,
-  };
-};
+//   return {
+//     isListening,
+//     startListening,
+//     stopListening,
+//   };
+// };
